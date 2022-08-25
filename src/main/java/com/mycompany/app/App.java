8a3f732760381eaa65d1ce5bb87b06c3a419dbe0
Amit-678 <amitsarkar4396@gmail.com>
2022-08-25 08:29:57 +0000

    Adding java project

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "Bhai Bhai, yeh project kaam kar raha hai!";
 
     public App() {}
 
